fab5e97c2ea86fd09a42b43a019f73d18f57c865
Rika <melty.dupre@gmail.com>
2024-11-03 17:21:06 -0500

    Removed unused Icon.

--- src/components/ui/map.tsx
+++ src/components/ui/map.tsx
@@ -7,7 +7,6 @@ import L from "leaflet";
 import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
-import { Icon } from "leaflet";
 
 interface Place {
   id: number;
